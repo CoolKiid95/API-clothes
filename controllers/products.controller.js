b9c8ce6dac5db8fe3d0fde5726ca117c94219cf9
Juan Santiago Fajardo Aldana <rallalpiso3@gmail.com>
2024-12-18 18:29:21 -0500

    dos funciones añadidas (getprodbycategory)

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -31,8 +31,9 @@ exports.addProduct=async(req,res)=>{
         let product = req.body
         let ownerid = req.usuario.id
         let roll = req.usuario.roll
+        
         if (roll == ("user")|| ("SuperAdmin")) {
-            
+
             product.owner = ownerid
             let newProduct = new productModel(product);
             await newProduct.save();
@@ -89,11 +90,24 @@ exports.getProductByOwner=async(req, res)=>{
     try {
         let owner = req.params.id
         if (owner.length==24) {
-            let myproducts = await productModel.find({owner:owner})
-            res.status(200).json(myproducts)
+            let owner = await productModel.find({owner:owner})
+            res.json(owner)
         }else{
             res.status(400).send({error:"ID incorrecto" });
         }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error:"Ha ocurrido un error, comunicate con el admin"})
+    }
+}
+
+exports.getProductByCategory=async(req, res)=>{
+    try {
+        let categoria = req.params.Category
+        console.log(categoria);
+        let products = await productModel.find({tipo:categoria}).limit(4)
+        res.json(products)
+
     } catch (error) {
         console.log(error);
         res.status(500).send({error:"Ha ocurrido un error, comunicate con el admin"})
