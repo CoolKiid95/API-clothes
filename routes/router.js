1ff694b47956fef62b0eb4446eb9ec06f5c178e9
CoolKiid95 <diegoal.delgadoc@gmail.com>
2024-12-20 13:53:42 -0500

    busqueda

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { tokenverification } = require('../middleware/jwt')
 //**** Ruta de los productos
 const productsController = require('../controllers/products.controller')
-router.get('/products', productsController.getProducts)
+router.get('/products/:nombre?', productsController.getProducts)
 router.get('/product/:id', productsController.getOneProduct)
 router.post('/addproduct',tokenverification, productsController.addProduct)
 router.delete('/deleteproduct/:id',tokenverification,productsController.deleteProduct)
